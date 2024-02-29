c340f578496373cf96b18ca06f877d958d6e051e
scirom <sebastian.cirom16@gmail.com>
2024-02-29 18:23:56 -0500

    Solución Números Pares

---
+++ NúmerosPares/Program.cs
@@ -0,0 +1,33 @@
+﻿//Construir un programa que pida un número y luego diga si este número es par o no.
+
+//Declaramos las variables
+
+using System.ComponentModel.Design;
+
+int num;
+double residuo;
+string mensaje;
+
+//Realizamos la solicitud del número en la consola
+
+Console.Write("Por favor digite un número mayor a 0: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+//Definimos la operación
+
+residuo = num % 2;
+
+//Agregamos el condicional
+
+if (residuo == 0)
+{
+    mensaje = "El número ingresado es par";
+}
+else
+{
+    mensaje = "El número ingresado es Impar";
+}
+
+//Imprimimos al valor resultante de la variable mensaje
+
+Console.Write(mensaje);
