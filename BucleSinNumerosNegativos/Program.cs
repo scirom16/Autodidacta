47d2caf0e9526801366f66184e044767efb1c337
scirom <sebastian.cirom16@gmail.com>
2024-03-01 21:57:00 -0500

    Solución 10mo algoritmo

---
+++ BucleSinNumerosNegativos/Program.cs
@@ -0,0 +1,24 @@
+﻿//Construir un algoritmo que pida N números por pantalla,
+//cuando el usuario ingrese un número negativo,
+//el programa debe finalizar y mostrar en consola la cantidad total de números positivos
+//que ingresó hasta antes de ingresarse el número negativo.
+
+//Declaramos variables
+int num;
+
+//Solicitamos el número por consola
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Por favor ingrese un número positivo");
+Console.ResetColor();
+num = Convert.ToInt32(Console.ReadLine());
+
+while (num >= 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Por favor ingrese un número positivo");
+    Console.ResetColor();
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Ingresaste un número negativo, haz salido del bucle");
+Console.ResetColor();
