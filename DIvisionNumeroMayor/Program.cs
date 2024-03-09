c65abf1c7fd152a576aa7d9af3baa6e8ee420807
scirom <sebastian.cirom16@gmail.com>
2024-03-08 19:25:52 -0500

    Solucion Numero Mayor

---
+++ DIvisionNumeroMayor/Program.cs
@@ -0,0 +1,36 @@
+﻿//Escribir un algoritmo que dados 2 valores de entrada imprima siempre la división del mayor entre el menor. Validar indeterminación
+
+//Declaramos las variables
+using System.ComponentModel.Design;
+
+double n1, n2;
+string mensaje = "La división es indeterminada";
+
+Console.WriteLine("Digite el primer número");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite el segundo número");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2)
+{
+    if(n2 == 0)
+    {
+        Console.WriteLine(mensaje);
+    }
+    else
+    {
+        Console.WriteLine(n1 / n2);
+    }
+}
+else if (n1 < n2)
+{
+    if(n1 == 0)
+    {
+        Console.WriteLine(mensaje);
+    }
+    else
+    {
+        Console.WriteLine(n2 / n1);
+    }
+}
---
+++ Pizzeria11/Program.cs
@@ -0,0 +1,27 @@
+﻿//La pizzería Bella Napoli ofrece pizzas vegetarianas y no vegetarianas a sus clientes.
+//Los ingredientes para cada tipo de pizza aparecen a continuación.
+//Ingredientes vegetarianos: Pimiento y tofu.
+//Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
+//Escribir un programa en C# que pregunte al usuario si quiere una pizza vegetariana o no,
+//y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija.
+//Solo se puede elegir un ingrediente además de la
+//mozzarella y el tomate que están en todas las pizzas.
+//Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
+
+Console.WriteLine("¿Qué pizza le gustaría comprar?");
+Console.WriteLine("para vegetariana digite 1, para no vegetariana digite 2");
+int pizza = Convert.ToInt32(Console.ReadLine());
+
+switch (pizza)
+{
+    case 1:
+        Console.WriteLine("Los ingredientes de la pizza vegetariana son:");
+        Console.WriteLine("Pimiento y Tofu");
+    break;
+    case 2:
+        Console.WriteLine("Los ingredientes de la pizza no vegetariana son:");
+        Console.WriteLine("Peperoni, Jamón y Salmón");
+        break;
+
+}
+Console.WriteLine("¿Qué ingrediente desea añadir a su pizza?" + pizza);
