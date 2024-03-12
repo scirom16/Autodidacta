991c5ce6174a2bca387a368b76e642a09e8999b3
scirom <sebastian.cirom16@gmail.com>
2024-03-11 22:50:46 -0500

    TryCatch (Para cuando el usuario ingrese un dato incorrecto)

---
+++ Mayor de tres numeros/Program.cs
@@ -0,0 +1,20 @@
+﻿//Declaramos variables
+int num1, num2, num3;
+
+Console.WriteLine("Por favor ingrese su primer número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Por favor ingrese su segundo número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Por favor ingrese su tercer número: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+int mayor = num1;
+
+if (num2 > mayor)
+{
+    mayor = num2;
+    if (num3 > mayor)
+    {
+        mayor = num3;
+    }
+}
+Console.WriteLine("El número mayor es: " + mayor);
---
+++ TryCatch/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    double precio, total;
+    int cantidad;
+
+    Console.WriteLine("Cantidad: ");
+    cantidad = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Precio: ");
+    precio = Convert.ToInt32(Console.ReadLine());
+
+    total = cantidad * precio;
+
+    Console.WriteLine("El total es: " + total);
+}
+catch(Exception error)
+{
+    Console.WriteLine("Ha ocurrido un error: "+ error.Message);
+}
